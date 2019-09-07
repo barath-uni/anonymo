bbd1b06c92ff87731815846de4308c0433a3d5ac
barathmonk <diputs.creators@gmail.com>
2019-09-07 22:09:58 +0530

    Day 1 commit - UI/Color Pallete

---
+++ lib/listdata.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ListData extends StatefulWidget {
+  @override
+  _ListDataState createState() => _ListDataState();
+}
+
+class _ListDataState extends State<ListData> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/userinput.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class UserInput extends StatefulWidget {
+  @override
+  _UserInputState createState() => _UserInputState();
+}
+
+class _UserInputState extends State<UserInput> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
